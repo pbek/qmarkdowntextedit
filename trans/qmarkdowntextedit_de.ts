82a749aa42b9d7a4d82765c11d8fbe9e5fb7cbb3
Patrizio Bekerle <office@bekerle.com>
2016-04-26 19:28:23 +0200

    added German translation

--- trans/qmarkdowntextedit_de.ts
+++ trans/qmarkdowntextedit_de.ts
@@ -4,31 +4,56 @@
 <context>
     <name>QTextEditSearchWidget</name>
     <message>
-        <location filename="../qtexteditsearchwidget.cpp" line="36"/>
+        <location filename="../qtexteditsearchwidget.ui" line="62"/>
         <source>close search</source>
         <translation>Suche schließen</translation>
     </message>
     <message>
-        <location filename="../qtexteditsearchwidget.cpp" line="42"/>
+        <location filename="../qtexteditsearchwidget.ui" line="52"/>
         <source>Find:</source>
         <translation>Finden:</translation>
     </message>
     <message>
-        <location filename="../qtexteditsearchwidget.cpp" line="47"/>
+        <location filename="../qtexteditsearchwidget.ui" line="32"/>
+        <source>replace text</source>
+        <translation>Text ersetzen</translation>
+    </message>
+    <message>
+        <location filename="../qtexteditsearchwidget.ui" line="79"/>
         <source>find in text</source>
         <translation>im Text finden</translation>
     </message>
     <message>
-        <location filename="../qtexteditsearchwidget.cpp" line="62"/>
+        <location filename="../qtexteditsearchwidget.ui" line="86"/>
         <source>search forward</source>
         <translation>vorwärts suchen
 rückwärts suchen
 vorwärts suchen</translation>
     </message>
     <message>
-        <location filename="../qtexteditsearchwidget.cpp" line="74"/>
+        <location filename="../qtexteditsearchwidget.ui" line="103"/>
         <source>search backward</source>
         <translation>rückwärts suchen</translation>
+    </message>
+    <message>
+        <location filename="../qtexteditsearchwidget.ui" line="120"/>
+        <source>replace with</source>
+        <translation>ersetzen mit</translation>
+    </message>
+    <message>
+        <location filename="../qtexteditsearchwidget.ui" line="127"/>
+        <source>Replace:</source>
+        <translation>Ersetzen:</translation>
+    </message>
+    <message>
+        <location filename="../qtexteditsearchwidget.ui" line="168"/>
+        <source>Replace</source>
+        <translation>Ersetzen</translation>
+    </message>
+    <message>
+        <location filename="../qtexteditsearchwidget.ui" line="178"/>
+        <source>Replace All</source>
+        <translation>Alle ersetzen</translation>
     </message>
 </context>
 </TS>
