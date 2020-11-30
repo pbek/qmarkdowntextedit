e2fe142cf86d7e33e2ed87bc070abce89a5789b3
Stefan Comanescu <fnkabit@gmail.com>
2020-11-30 17:22:49 +0200

    Add new method for retrieving range of codespan (#131)
    * Add new method for retrieving range of codespan
    
    * Address review remarks

--- markdownhighlighter.cpp
+++ markdownhighlighter.cpp
@@ -2196,6 +2196,23 @@ bool MarkdownHighlighter::isPosInACodeSpan(int blockNumber, int position) const
     }) != rangeList.cend();
 }
 
+QPair<int, int> MarkdownHighlighter::getSpanRange(MarkdownHighlighter::RangeType rangeType, int blockNumber, int position) const
+{
+    const QVector<InlineRange> rangeList = _ranges.value(blockNumber);
+    const auto it = std::find_if(rangeList.cbegin(), rangeList.cend(),
+                                     [position, rangeType](const InlineRange& range){
+        if (position > range.begin && position < range.end && range.type == rangeType)
+            return true;
+        return false;
+    });
+
+    if (it == rangeList.cend()) {
+        return QPair<int, int>(-1, -1);
+    } else {
+        return QPair<int, int>(it->begin, it->end);
+    }
+}
+
 /**
  * @brief highlights Em/Strong in text editor
  */
