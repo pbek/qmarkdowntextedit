58fb8ca3b3086b3e288ed55240241b9ade371db3
Patrizio Bekerle <patrizio@bekerle.com>
2021-12-29 11:49:25 +0100

    Update year to 2022

--- markdownhighlighter.cpp
+++ markdownhighlighter.cpp
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2014-2021 Patrizio Bekerle -- <patrizio@bekerle.com>
+ * Copyright (c) 2014-2022 Patrizio Bekerle -- <patrizio@bekerle.com>
  * Copyright (c) 2019-2021 Waqar Ahmed      -- <waqar.17a@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
